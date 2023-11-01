afd5b861772625996476ebc95a73714014f906ce
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-10-31 22:48:47 -0400

    move todoList into state and control Add Todo Input

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 
 function AddTodoForm(props) {
- 
+  
+  const [todoTitle, setTodoTitle] = useState([])
+  
+  const handleTitleChange = (event) => {
+    event.preventDefault();
+    
+    let newTodoTitle = event.target.value
+    setTodoTitle(newTodoTitle)
+  }
+
   const handleAddTodo = (event) => {
     event.preventDefault();
     
-    let todoTitle = event.target.title.value;
-    props.onAddTodo(todoTitle)
+    let newTodoTitle = event.target.title.value;
+    props.onAddTodo([newTodoTitle])
+
+    
   }
 
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle"> Title </label>
-      <input type="text" id="todoTitle" name="title"></input>
+      <input  value={todoTitle} onChange={handleTitleChange} type="text" id="todoTitle" name="title"/>
       <button type="submit"> Add</button>
     </form>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,33 +1,12 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const todoList = [
-  {
-    id: 0,
-    title: "start assignment",
-    complete:"Yes"
-  },
-  {
-    id: 1,
-    title: "Work on assignment",
-    complete:"Yes"
-  },
-  {
-    id: 2,
-    title: "Complete assignment",
-    complete:"Yes"
-  },
-  {
-    id:3,
-    title: "TodoListItem week 3",
-    complete:"No"
-  }
-];
 
-const TodoList = () => {
+
+const TodoList = (props) => {
   return (
     <ul>
-      {todoList.map(function (item) {
+      {props.todoList.map(function (item) {
         return (
           
             <TodoListItem  key={item.id} todo={item} complete={item.complete}/> 
