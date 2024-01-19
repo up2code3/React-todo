e79d60fa76da4098da0b0cbfb8fa5907de498040
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-01-18 23:13:51 -0500

    put routes back no work but no error

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import InputWithLabel from "./InputWithLabel";
 import style from "./mainStyle.module.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function AddTodoForm({onAddTodo}) {
   
@@ -23,17 +24,34 @@ function AddTodoForm({onAddTodo}) {
   }
 
   return (
-    <form onSubmit={handleAddTodo} className={style.textColor}>
-    <InputWithLabel 
-      id="todoTitle" 
-      value={todoTitle} 
-      onChange={handleTitleChange} 
-      type="text"  
-    > 
-      Title
-    </InputWithLabel>
-      <button type="submit"> Add</button>
-    </form>
+    <BrowserRouter>
+      <Routes>
+        <Route
+        path="/"
+        element={
+        <form onSubmit={handleAddTodo} className={style.textColor}>
+          <InputWithLabel 
+            id="todoTitle" 
+            value={todoTitle} 
+            onChange={handleTitleChange} 
+            type="text"  
+          > 
+          Title
+        </InputWithLabel>
+          <button type="submit"> Add</button>
+        </form>
+        }
+        />
+        <Route
+        path="/new"
+        element={
+          <>
+          <h1>BackBurner</h1>
+          </>
+        }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
