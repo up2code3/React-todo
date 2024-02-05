34137f94dd2911e53a4fdbf657eeec9062ebd9f8
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-02-04 20:44:22 -0500

    merged and working

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,1 +1,41 @@
-c
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const InputWithLabel = ({
+  id,
+  isFocused,
+  children,
+  value,
+  type,
+  name,
+  onChange,
+}) => {
+  const inputRef = React.useRef();
+  React.useEffect(() => {
+    inputRef.current.focus();
+  });
+  return (
+    <>
+      <label htmlFor={id}> {children} </label>
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChange}
+        type={type}
+        id={id}
+        name="title"
+      />
+    </>
+  );
+};
+
+InputWithLabel.propTypes = {
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default InputWithLabel;
+
