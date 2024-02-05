ce77c2af46fc6eba91341707cb264df83309a0ca
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-02-04 19:56:47 -0500

    all good

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,146 +1,147 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from "prop-types"
-import AddTodoForm from './AddTodoForm'
-import TodoList from './TodoList'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 
+const TodoContainer = ({ REACT_APP_TABLE_NAME }) => {
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAscending, setIsAscending] = useState(true);
 
-const TodoContainer = ({REACT_APP_TABLE_NAME }) => {
-    const [todoList, setTodoList] = useState([])
-    const [isLoading, setIsLoading] = useState(true);
-    const [isAscending, setIsAscending] = useState(true); 
+  useEffect(() => {
+    const fetchDataAndSort = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+        },
+      };
 
-    useEffect(() => {
+      const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`;
 
-        const fetchDataAndSort = async () => {
-            const options = {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
-                },
-            };
+      try {
+        const response = await fetch(url, options);
 
-            const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`;
-            
-            try {
-                const response = await fetch(url, options);
-
-                if(!response.ok) {
-                    throw new Error(`Error: ${response.status}`)
-                }
-
-                const data = await response.json();
-
-                data.records.sort((objectA, objectB) => {
-                    const titleA = objectA.fields.title.toUpperCase();
-                    const titleB = objectB.fields.title.toUpperCase();
-                    const comparison = isAscending ? 1 : -1;
-
-                    if (titleA < titleB) {
-                        return comparison;
-                    } else if (titleA > titleB){
-                        return -comparison;
-                    } else {
-                        return 0;
-                    }
-                });
-
-                const todos = data.records.map((record) => ({
-                    title: record.fields.title,
-                    id: record.id,
-                }));
-
-                setTodoList(todos);
-                setIsLoading(false);
-            } catch (error) {
-                console.error('Fetch Error', error.message);
-            }
-        };
-        
-        fetchDataAndSort();
-            
-    }, [REACT_APP_TABLE_NAME, isAscending]);
-
-    const toggleSortingOrder = () => {
-        setIsAscending((prevIsAscending) => !prevIsAscending);
-        // fetchDataAndSort();
-    };
-
-    const addTodo = async (todo) => {
-        const options = {
-         method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization:  `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
-            },
-            body: JSON.stringify({
-                fields: {
-                    title: todo.title,
-                },
-            }),
-        };
-
-        const url =  `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}`;
-        
-        try {
-            const response = await fetch(url, options);
-
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-
-            const newRecord = await response.json();
-
-            setTodoList([...todoList, newRecord.fields]);
-        } catch (error) {
-            console.error('Add Todo Error:', error.message);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
         }
+
+        const data = await response.json();
+
+        data.records.sort((objectA, objectB) => {
+          const titleA = objectA.fields.title.toUpperCase();
+          const titleB = objectB.fields.title.toUpperCase();
+          const comparison = isAscending ? 1 : -1;
+
+          if (titleA < titleB) {
+            return comparison;
+          } else if (titleA > titleB) {
+            return -comparison;
+          } else {
+            return 0;
+          }
+        });
+
+        const todos = data.records.map((record) => ({
+          title: record.fields.title,
+          id: record.id,
+        }));
+
+        setTodoList(todos);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Fetch Error", error.message);
+      }
     };
 
-    const removeTodo = async (id) => {
-        const options = {
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
-            },
-        };
+    fetchDataAndSort();
+  }, [REACT_APP_TABLE_NAME, isAscending]);
 
-        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}/${id}`;
-        
-        try {
-            const response = await fetch(url, options);
+  const toggleSortingOrder = () => {
+    setIsAscending((prevIsAscending) => !prevIsAscending);
+    // fetchDataAndSort();
+  };
 
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-
-            //Parse the json
-            await response.json();
-
-            //set todoList state to new Array not containting removed record
-            const updatedTodoList = todoList.filter((todo) => todo.id !== id);
-            setTodoList(updatedTodoList);
-        } catch (error) {
-            console.error('Remove Todo Error:', error.message);
-        }
+  const addTodo = async (todo) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+      body: JSON.stringify({
+        fields: {
+          title: todo.title,
+        },
+      }),
     };
 
-    return (
-        <div>
-            <h1>{REACT_APP_TABLE_NAME}</h1>
-            
-            <AddTodoForm onAddTodo={addTodo}/>
-            
-            <button onClick={toggleSortingOrder}>
-                Toggle Sorting Order {isAscending ? 'Ascending' : 'Descending'}
-            </button>
-            <hr/>
-            {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
-        </div>
-    );
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const newRecord = await response.json();
+
+      setTodoList([...todoList, newRecord.fields]);
+    } catch (error) {
+      console.error("Add Todo Error:", error.message);
+    }
+  };
+
+  const removeTodo = async (id) => {
+    const options = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+    };
+
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${REACT_APP_TABLE_NAME}/${id}`;
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      //Parse the json
+      await response.json();
+
+      //set todoList state to new Array not containting removed record
+      const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+      setTodoList(updatedTodoList);
+    } catch (error) {
+      console.error("Remove Todo Error:", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>{REACT_APP_TABLE_NAME}</h1>
+
+      <AddTodoForm onAddTodo={addTodo} />
+
+      <button onClick={toggleSortingOrder}>
+        Toggle Sorting Order {isAscending ? "Ascending" : "Descending"}
+      </button>
+      <hr />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
+    </div>
+  );
 };
 
 TodoContainer.propTypes = {
-    REACT_APP_TABLE_NAME: PropTypes.string.isRequired,
+  REACT_APP_TABLE_NAME: PropTypes.string.isRequired,
 };
 
 export default TodoContainer;
