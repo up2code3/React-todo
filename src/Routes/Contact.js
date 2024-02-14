f26a0f8b1f9ee4056ce4c8861d387e71a5f24210
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-02-13 19:34:07 -0500

    mm

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
             <h1>Contact</h1>
             <p> For more info contact: <a href="mailto:the_position3030@yahoo.com">the_position3030@yahoo.com</a> </p>
             <img
-            src="https://assets.bonappetit.com/photos/6491b45d047251c7e5ee269b/16:9/w_1280,c_limit/GettyImages-107806725.jpg"
+            src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/01/colorful-supermarket-aisle-withe-no-791693291-2.jpg?w=620"
             className={`${style.cartPic}`}
             alt="ShopCartPic"
             />
