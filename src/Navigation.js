73cba342166f2f2df199ebc23a724c0c6f123bfa
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-02-17 11:01:01 -0500

    add rel noreferrer for target blk security risk

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,8 +5,9 @@ function Navigation() {
     return (
         <nav className={style.nav}>
             <ul>
+                
                 <li><a href="/" className={style.navLink}>Home</a></li>
-                <li> <a target="_blank" href="https://shorturl.at/aBY46" className={style.navLink}>airtable</a> </li>
+                <li> <a target="_blank" rel="noreferrer" href="https://shorturl.at/aBY46" className={style.navLink}>airtable</a> </li>
                 <li> <a href="/contact" className={style.navLink}>contact</a> </li>
             </ul>
         </nav>
